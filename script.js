33aa32b7207c5cafe6eea7330170e91942c0ef45
Mn <mmhdhnfii@gmail.com>
2026-05-10 23:22:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+// COUNTER
+let count = 0;
+
+const counter = document.getElementById("counter");
+
+const interval = setInterval(() => {
+
+  count += 7;
+
+  if(count >= 2009){
+    count = 2009;
+    clearInterval(interval);
+  }
+
+  counter.textContent = count;
+
+}, 20);
+
+
+// THEME
+function toggleTheme(){
+  document.body.classList.toggle("dark");
+}
+
+
+// HOME
+function goHome(){
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
+}
+
+
+// GALLERY
+function openGallery(){
+
+  document.getElementById("gallery")
+  .scrollIntoView({
+    behavior: "smooth"
+  });
+
+}
+
+
+// CONTACT MODAL
+const contactModal =
+document.getElementById("contactModal");
+
+function openContact(){
+  contactModal.style.display = "flex";
+}
+
+function closeContact(){
+  contactModal.style.display = "none";
+}
+
+
+// MUSIC
+const bgMusic =
+document.getElementById("bgMusic");
+
+function toggleMusic(){
+
+  if(bgMusic.paused){
+    bgMusic.play();
+  } else {
+    bgMusic.pause();
+  }
+
+}
+
+
+// INFO MODAL
+function openModal(title, text){
+
+  document.getElementById("modal")
+  .style.display = "flex";
+
+  document.getElementById("modal-title")
+  .innerText = title;
+
+  document.getElementById("modal-text")
+  .innerText = text;
+
+}
+
+function closeModal(){
+
+  document.getElementById("modal")
+  .style.display = "none";
+
+}
+
+
+// CLOSE MODAL OUTSIDE
+window.addEventListener("click", (e) => {
+
+  if(e.target.classList.contains("modal")){
+    e.target.style.display = "none";
+  }
+
+});
+
+
+// INSTALL APP
+let deferredPrompt;
+
+const installBtn =
+document.getElementById("installBtn");
+
+installBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", (e) => {
+
+  e.preventDefault();
+
+  deferredPrompt = e;
+
+  installBtn.style.display = "block";
+
+});
+
+installBtn.addEventListener("click", async () => {
+
+  if(deferredPrompt){
+
+    deferredPrompt.prompt();
+
+  }
+
+});
+if("serviceWorker" in navigator){
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker.register("./sw.js")
+    .then(() => {
+      console.log("SW aktif");
+    });
+
+  });
+
+}
